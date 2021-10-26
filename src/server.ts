23e13c263ac0760afbba5c1ab32913a799c27993
Anke <pfeiffer.anke@outlook.com>
2021-10-26 11:36:57 +0200

    Return :name

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 
-const app = express();
-const port = 3000;
+const app = express(); // erstelle App
+const port = 3000; // lokaler offener Port
+
+app.get('/api/users/:name/', (request, response) => {
+  response.send(request.params.name);
+});
+
+app.get('/api/users', (_request, response) => {
+  const users = ['David', 'Anke', 'Alice', 'Zied'];
+  response.send(users); // schickt Inhalt des Arrays zurück
+});
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  // GET request to root directory (/)
+  res.send('Hello World!'); // Server soll "Hello World" zurückschicken
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// user route anlegen
+// wie kann man auf den Namen zugreifen? -> route parameters
