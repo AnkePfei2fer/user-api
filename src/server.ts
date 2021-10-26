cd22d1de5707db37498e97c0a99b2b768a3f2253
Anke <pfeiffer.anke@outlook.com>
2021-10-26 16:04:24 +0200

    Delete user

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
 import express from 'express';
 
 const app = express(); // create app
-const port = 3000; // local open port
+const port = 3000; // use local open port
 
 const users = ['David', 'Anke', 'Alice', 'Zied'];
+
+app.delete('/api/users/:name', (request, response) => {
+  const isNameKnown = users.includes(request.params.name);
+  if (isNameKnown) {
+    const index = users.indexOf(request.params.name);
+    users.splice(index, 1);
+    response.send(
+      'User ' + request.params.name + ' deleted' + '. Still in: ' + users
+    );
+  } else {
+    response.status(404).send('Name is unknown');
+  }
+});
 
 app.get('/api/users/:name/', (request, response) => {
   const isNameKnown = users.includes(request.params.name);
   if (isNameKnown) {
     response.send(request.params.name);
   } else {
-    response.status(404).send('Name is unknown'); // Console gibt zusätzlich den Status aus
+    response.status(404).send('Name is unknown'); // status is displayed in console
   }
 });
 
@@ -26,6 +39,3 @@ app.get('/', (_req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-// user route anlegen
-// wie kann man auf den Namen zugreifen? -> route parameters
